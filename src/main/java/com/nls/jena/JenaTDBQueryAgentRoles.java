4a34718617f95c2206a331f5c9699e13fea1ef3d
hibernator11 <gustavo.candela@cerrvantesvirtual.com>
2022-12-08 20:30:23 +0100

    analysis roles

--- src/main/java/com/nls/jena/JenaTDBQueryAgentRoles.java
+++ src/main/java/com/nls/jena/JenaTDBQueryAgentRoles.java
@@ -40,7 +40,8 @@ public class JenaTDBQueryAgentRoles {
 
             qe = QueryExecutionFactory.create("PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
                     "SELECT distinct ?l " +
-                    "WHERE {?s a <http://id.loc.gov/ontologies/bibframe/Role> . ?s rdfs:label ?l} limit 10", dataset);
+                    "WHERE {?s a <http://id.loc.gov/ontologies/bibframe/Role> . ?s rdfs:label ?l} " +
+                    "limit 100", dataset);
             for (ResultSet results = qe.execSelect(); results.hasNext();) {
                 QuerySolution qs = results.next();
                 String lValue = qs.get("?l").toString();
@@ -74,6 +75,34 @@ public class JenaTDBQueryAgentRoles {
                 logger.info("loc relators total:" + roleValue);
             }
 
+            qe = QueryExecutionFactory.create("PREFIX bf:<http://id.loc.gov/ontologies/bibframe/> " +
+                    "SELECT distinct ?role " +
+                    "WHERE {?s bf:contribution ?c . " +
+                    " ?c bf:role ?role ." +
+                    " filter (!regex(str(?role), \"relator\"))" +
+                    "} limit 20", dataset);
+            for (ResultSet results = qe.execSelect(); results.hasNext();) {
+                QuerySolution qs = results.next();
+                String roleValue = qs.get("?role").toString();
+
+                logger.info("roles text:" + roleValue);
+            }
+
+            // check how the roles are mapped composer lyricist
+            qe = QueryExecutionFactory.create("PREFIX bf:<http://id.loc.gov/ontologies/bibframe/> " +
+                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+                    "SELECT distinct ?role ?label " +
+                    "WHERE {<http://example.org/9958287493804341#Work> bf:contribution ?c . " +
+                    " ?c bf:role ?role ." +
+                    " ?role rdfs:label ?label " +
+                    "} limit 20", dataset);
+            for (ResultSet results = qe.execSelect(); results.hasNext();) {
+                QuerySolution qs = results.next();
+                String roleValue = qs.get("?role").toString();
+                String lValue = qs.get("?label").toString();
+
+                logger.info("http://example.org/9958287493804341#Work role text:" + roleValue + " " + lValue);
+            }
 
 
             // Releasing dataset resources
